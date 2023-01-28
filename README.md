0443ba7672ab21fcc02e70fb4b83630b472001ec
Kimberly Cable <cableredi@comcast.net>
2023-01-28 12:42:06 -0700

    complete remodification

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # Welcome to my Portfolio
 
-## Summary
-Here you will find projects that I have done and the skills that I have.
+## About
+Hi, I'm a Data Scientist and Developer based in Denver, CO.
+<br />
+My background is in software development but the world of data just pulled me in.
 
-## [Check it out...](https://cableredi.github.io/kcportfolio/)
+## Summary
+Here you will find projects that I have done and the skills that I have aquired.
+
+## Skills Used
+ETL, Data Wrangling, Data Cleaning, Data Visualization, Web Scraping, Predictive Analysis, Machine Learning, Natural Language Processing, Computer Vision, Deep Learning, and more
 
 ## Technologies Used
-HTML, SCSS, JavaScript, jQuery
+Python3, R, pandas, matplotlib, seaborn, skikit-learn, numpy, SQL, Tableau, and more
+
+## [View Portfolio...](https://cableredi.github.io/kcportfolio/)
+
+## Projects
+
+* ### Data Wrangling
+
+* ### Customer Segmentation
+
+* ### Data Visualization
+
+* ### Chocolate Prediction
+
+* ### Computer Vision - Chatbot
+
+* ### Computer Vision - Learn to Write your Letters
+
+* ### Weather Forecast
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,37 +1,90 @@
-/* scripts */
+// Get each modal and close button
+const triggers = document.getElementsByClassName("trigger");
+const triggerArray = Array.from(triggers).entries();
+const modals = document.getElementsByClassName("modal");
+const closeButtons = document.getElementsByClassName("close-button");
 
-/* get current year */
-document.getElementById("year").innerHTML = new Date().getFullYear();
+let modal01 = document.getElementById('modal01');
+let modal02 = document.getElementById('modal02');
+let modal03 = document.getElementById('modal03');
+let modal04 = document.getElementById('modal04');
+let modal05 = document.getElementById('modal05');
+let modal06 = document.getElementById('modal06');
 
-/* side nav */
-function openNav() {
-  document.getElementById("navbar__sidenav").style.width = "130px";
-}
-
-function closeNav() {
-  document.getElementById("navbar__sidenav").style.width = "0";
-}
-
-/* Modal */
-let modal = document.getElementsByClassName("main__projects-project-modal");
-let btn = document.getElementsByClassName("main__projects-project-info");
-let span = document.getElementsByClassName(
-  "main__projects-project-modal-content-close"
-);
-
-for (let i = 0; i < modal.length; i++) {
-  btn[i].onclick = function () {
-    modal[i].style.display = "block";
+// Then use `for...of`-loop with the index of each item in `triggerArray` for listening to a click event which toggles each modal to open and close
+for (let [index, trigger] of triggerArray) {
+  function toggleModal() {
+    modals[index].classList.toggle("show-modal");
   };
 
-  span[i].onclick = function () {
-    modal[i].style.display = "none";
-  };
+  trigger.addEventListener("click", toggleModal);
+  closeButtons[index].addEventListener("click", toggleModal);
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target === modal) {
-    modal.style.display = "none";
+window.onclick = function(event) {
+  if (event.target == modal01) {
+      modal01.style.display = "none";
   }
-};
+  if (event.target == modal02) {
+    modal02.style.display = "none";
+  }
+  if (event.target == modal03) {
+    modal03.style.display = "none";
+  }
+  if (event.target == modal04) {
+    modal04.style.display = "none";
+  }
+  if (event.target == modal05) {
+    modal05.style.display = "none";
+  }
+  if (event.target == modal06) {
+    modal06.style.display = "none";
+  }
+}
+
+/* Highlight Menu Item on Scoll or Click */
+
+offset = 100;
+$('#menu li a[href^="#"]').click(function(event) {
+
+    // Prevent from default action to intitiate
+    event.preventDefault();
+      
+    //remove active from all anchor and add it to the clicked anchor
+    $('#menu li a[href^="#"]').removeClass("active")
+    $(this).addClass('active');
+    
+    // The id of the section we want to go to
+    var anchorId = $(this).attr('href');
+
+    // Our scroll target : the top position of the section that has the id referenced by our href
+    var target = $(anchorId).offset().top - offset;
+    //console.log(target);
+
+    $('html, body').stop().animate({ scrollTop: target }, 500, function () {
+        window.location.hash = anchorId;
+    });
+
+    return false;
+});
+
+//check the pages when scroll event occurs
+$(window).scroll(function(){
+    // Get the current vertical position of the scroll bar
+    position = $(this).scrollTop();
+    $('#menu li a[href^="#"]').each(function(){
+          var anchorId = $(this).attr('href'); 
+           var target = $(anchorId).offset().top - offset;
+           // check if the document has crossed the page
+        console.log(position,target);
+        if(position>=target){
+             //remove active from all anchor and add it to the clicked anchor
+            $('#menu li a[href^="#"]').removeClass("active")
+            $(this).addClass('active'); 
+        }
+    })
+})
+$(function(){
+    //set our scroll state after dom ready
+    $(window).scroll();
+})
